58c1dae53d28ac883aed285d4c5df669f5cd0f36
Akira Takagi <tkgakrwork@gmail.com>
2026-06-27 15:35:06 +0900

    doc: コメント追加

--- src/api/infrastructure/database/dbAccess.ts
+++ src/api/infrastructure/database/dbAccess.ts
@@ -11,7 +11,7 @@ export const getDbInstance = (): NodePgDatabase => {
   return dbInstance
 }
 
-// テスト等で新規接続したいとき用
+// テスト等で新規接続したいときはこっちを直接コール
 export const createDbConnection = (): NodePgDatabase => {
   const appConfig = loadEnv()
   return drizzle({
@@ -20,7 +20,10 @@ export const createDbConnection = (): NodePgDatabase => {
       min: appConfig.database.poolMin,
       max: appConfig.database.poolMax,
     }),
+    // Drizzle が camelCase のプロパティ名を snake_case のカラム名へ自動変換する
+    // https://orm.drizzle.team/docs/sql-schema-declaration#camel-and-snake-casing
     casing: 'snake_case',
+    // true の時、実行SQL をログに出力
     logger: appConfig.system.debug,
   })
 }
