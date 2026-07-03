3dcfef1b6c9c8665df67f512b586fe5b33383703
Akira Takagi <tkgakrwork@gmail.com>
2026-07-03 22:06:57 +0900

    style: 改行調整

--- src/api/application/service/book/bookCheckService.ts
+++ src/api/application/service/book/bookCheckService.ts
@@ -15,7 +15,10 @@ export const checkBookTitleExists = (
   // タイトル未指定（空白）なら何もチェックしない
   return isEmptyOrWhitespaceString(title)
     ? okAsync()
-    : ResultAsync.fromPromise(repository.findByTitle(title), promiseErrorReturn(logger, ResultCodes.BOOK_DUPLICATE_CHECK_FAILED)).andThen((result) =>
+    : ResultAsync.fromPromise(
+        repository.findByTitle(title),
+        promiseErrorReturn(logger, ResultCodes.BOOK_DUPLICATE_CHECK_FAILED),
+      ).andThen((result) =>
         // 更新/復元では自分自身を除外して判定
         isEmptyArray(excludeId ? result.value.filter((item) => item.id !== excludeId) : result.value)
           ? okAsync()
