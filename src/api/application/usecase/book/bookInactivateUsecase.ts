59aa33757d41dfd8082752c9cb7851905de309cc
Akira Takagi <tkgakrwork@gmail.com>
2026-07-05 14:45:00 +0900

    feat: 本の無効化

---
+++ src/api/application/usecase/book/bookInactivateUsecase.ts
@@ -0,0 +1,22 @@
+import { falsyValueCheck, nullOrUndefinedCheck, promiseErrorReturn } from '@application/service/generic/utility'
+import { inactivateBook } from '@domain/model/book/book'
+import type { OperationResult } from '@domain/model/generic/generic'
+import { ResultCodes } from '@domain/model/generic/generic'
+import type { IBookRepository } from '@domain/repository/book/bookRepository'
+import type { ILogger } from '@domain/service/logger/iLoggerService'
+import type { Result } from 'neverthrow'
+import { ResultAsync } from 'neverthrow'
+
+export const bookInactivateUsecase = async (
+  repository: IBookRepository,
+  logger: ILogger,
+  bookId: string,
+): Promise<Result<boolean, OperationResult>> => {
+  return ResultAsync.fromPromise(repository.fetchDetail(bookId), promiseErrorReturn(logger, ResultCodes.BOOK_FETCH_FAILED))
+    .map((entity) => entity.value)
+    .andThen(nullOrUndefinedCheck(ResultCodes.BOOK_NOT_FOUND))
+    .andThen((entity) =>
+      ResultAsync.fromPromise(repository.save(inactivateBook(entity.id)), promiseErrorReturn(logger, ResultCodes.BOOK_SAVE_FAILED)),
+    )
+    .andThen(falsyValueCheck(ResultCodes.BOOK_SAVE_FAILED))
+}
