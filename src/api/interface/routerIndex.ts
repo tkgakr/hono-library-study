0b74c952dc88fee052709a2be265ae4259aac743
Akira Takagi <tkgakrwork@gmail.com>
2026-07-29 19:12:15 +0900

    feat: memberRouteのマウントを追加

--- src/api/interface/routerIndex.ts
+++ src/api/interface/routerIndex.ts
@@ -1,5 +1,7 @@
 import { createOpenApiHono } from '@infrastructure/middleware/honoOpenApiFactory'
 import { bookRoute } from '@interface/router/bookRouter'
+import { memberRoute } from '@interface/router/memberRouter'
 
 export const apiRouter = createOpenApiHono()
 apiRouter.route('/books', bookRoute)
+apiRouter.route('/members', memberRoute)
